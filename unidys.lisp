0defd068408396d47c6556a693144664181d948e
Andreas Nilsson <andreas.nilsson@jayway.com>
2022-02-05 00:58:41 +0100

    Reorg.

--- unidys.lisp
+++ unidys.lisp
@@ -36,6 +36,21 @@
 (defmethod unidys-db:model-table ((self user))
   (find-table 'users))
 
+(defstruct (cap (:include model))
+  (rc (new-model-proxy (find-table 'rcs) 'rc) :type model-proxy)
+  (starts-at (error "missing starts-at") :type timestamp)
+  (ends-at (error "missing ends-at") :type timestamp)
+  (total (error "missing total") :type integer)
+  (used (error "missing used") :type integer))
+
+(defun new-cap (rc starts-at ends-at total used)
+  (let ((self (make-cap :starts-at starts-at :ends-at ends-at :total total :used used)))
+    (set-model (cap-rc self) rc)
+    self))
+
+(defmethod unidys-db:model-table ((self cap))
+  (find-table 'caps))
+
 (defstruct (rc (:include model))
   (name "" :type string)
   (created-at (now) :type timestamp)
@@ -49,23 +64,8 @@
 (defmethod unidys-db:model-table ((self rc))
   (find-table 'rcs))
 
-(defstruct (cap (:include model))
-  (rc (new-model-proxy (find-table 'rcs) 'rc) :type model-proxy)
-  (starts-at (error "missing starts-at") :type timestamp)
-  (ends-at (error "missing ends-at") :type timestamp)
-  (total (error "missing total") :type integer)
-  (used (error "missing used") :type integer))
-
-(defun new-cap (rc starts-at ends-at total used)
-  (let ((self (make-cap :starts-at starts-at :ends-at ends-at :total total :used used)))
-    (set-model (cap-rc self) rc)
-    self))
-
 (defmethod unidys-db:model-store :after ((self rc))
   (model-store (new-cap self *min-timestamp* *max-timestamp* 0 0)))
-
-(defmethod unidys-db:model-table ((self cap))
-  (find-table 'caps))
 
 (defmacro with-db ((&rest args) &body body)
   `(with-cx (,@args)
